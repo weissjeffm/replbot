07d789cca96b5d560f592ef114e483a29823d509
Jeff Weiss <me@jweiss.com>
2016-03-19 15:23:15 -0400

    Initial code with eval, help, and karma.
    Config is still hardcoded. No state persistence.
    
    Currently depends on having custom xmpp lib (modified quit-yo-jibber) in
    checkouts. Need to move xmpp stuff into this repository.

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject replbot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;;[quit-yo-jibber "0.6.0"]
+                 [org.igniterealtime.smack/smack-core "4.1.6"]
+                 [org.igniterealtime.smack/smack-tcp "4.1.6"]
+                 [org.igniterealtime.smack/smack-extensions "4.1.6"]
+                 [org.igniterealtime.smack/smack-java7 "4.1.6"]
+                 [org.igniterealtime.smack/smack-debug "4.1.6"]
+                 [clojail "1.0.4"]])
